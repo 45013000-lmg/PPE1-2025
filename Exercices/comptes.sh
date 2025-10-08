32be0baa7b82b8cfe9d7be60f34db56e4df49570
Mengge LIU <45013000-parisnanterre.fr>
2025-10-08 12:37:28 +0200

    Exercice 1 Un premier script

---
+++ Exercices/comptes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "2016 $(cat /home/menggeliu/Exercice1/ann/2016/* | grep Location | wc -l)"
+echo "2017 $(cat /home/menggeliu/Exercice1/ann/2017/* | grep Location | wc -l)"
+echo "2018 $(cat /home/menggeliu/Exercice1/ann/2018/* | grep Location | wc -l)"
